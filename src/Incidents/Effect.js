2835dfbc3004228bc14c565c10d7679e2c1cf8d8
ivanvl01 <1v4nvl@gmail.com>
2021-10-27 09:27:43 +0000

    Deploying to gh-pages from @ donkeyclip/motorcortex-waa@6653652c7f929bfd85724728a9457874f50f418b 🚀

--- src/Incidents/Effect.js
+++ src/Incidents/Effect.js
@@ -1,12 +1,13 @@
-import MotorCortex from "@kissmybutton/motorcortex";
+import MotorCortex from "@donkeyclip/motorcortex";
 import compoAttributes from "../compoAttributes";
-
+import getMatrix2D from "./matrix2d";
 
 export default class WAA extends MotorCortex.Effect {
-
   getScratchValue() {
     if (this.attributeKey !== "transform") {
-      return anime.get(this.element, this.attributeKey);
+      return this.context.window.getComputedStyle(this.element)[
+        this.attributeKey
+      ];
     }
 
     const obj = {};
@@ -19,7 +20,7 @@ export default class WAA extends MotorCortex.Effect {
         transform[i]
       )
         ? currentTransform[transform[i]]
-        : anime.get(this.element, transform[i]);
+        : this.context.window.getComputedStyle(this.element)[transform[i]];
     }
 
     return obj;
@@ -31,7 +32,7 @@ export default class WAA extends MotorCortex.Effect {
       Object.prototype.hasOwnProperty.call(compoAttributes, this.attributeKey)
     ) {
       const compoAttribute = compoAttributes[this.attributeKey];
-      options.transform=[]
+      options.transform = [];
       for (let i = 0; i < compoAttribute.length; i++) {
         if (
           !Object.prototype.hasOwnProperty.call(
@@ -42,19 +43,22 @@ export default class WAA extends MotorCortex.Effect {
           continue;
         }
         options.transform = [
-          `${options.transform[0] || ''} ${[compoAttribute[i]]}(${this.initialValue[compoAttribute[i]]})`
-          ,
-          `${options.transform[1] || ''} ${[compoAttribute[i]]}(${this.targetValue[compoAttribute[i]]})`
+          `${options.transform[0] || ""} ${[compoAttribute[i]]}(${
+            this.initialValue[compoAttribute[i]]
+          })`,
+          `${options.transform[1] || ""} ${[compoAttribute[i]]}(${
+            this.targetValue[compoAttribute[i]]
+          })`,
         ];
-
-       
       }
     } else {
       options[this.attributeKey] = [this.initialValue, this.targetValue];
     }
-    console.log(options)
     this.target = this.element.animate(
-      [{ [this.attributeKey]: options[this.attributeKey][0] }, { [this.attributeKey]:  options[this.attributeKey][1] }],
+      [
+        { [this.attributeKey]: options[this.attributeKey][0] },
+        { [this.attributeKey]: options[this.attributeKey][1] },
+      ],
       {
         duration: this.props.duration,
         fill: "forwards",
@@ -62,12 +66,9 @@ export default class WAA extends MotorCortex.Effect {
       }
     );
     this.target.pause();
-
   }
 
-
-  onProgress(fraction, millisecond) {
+  onProgress(fraction) {
     this.target.currentTime = this.props.duration * fraction;
-     
   }
 }
