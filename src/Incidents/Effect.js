67c47b6b7606e30faf2703b9d2acabd0c4424d08
Theofilos Papadopoulos <theofilos.p@kissmybutton.gr>
2021-11-15 03:39:17 +0200

    fix: multiple incidents with same attribute on same element

--- src/Incidents/Effect.js
+++ src/Incidents/Effect.js
@@ -2,62 +2,62 @@ import MotorCortex from "@donkeyclip/motorcortex";
 import compoAttributes from "../compoAttributes";
 import getMatrix2D from "./matrix2d";
 
+const { hasOwnProperty } = Object.prototype;
+
 export default class WAA extends MotorCortex.Effect {
   getScratchValue() {
+    const { getComputedStyle } = this.context.window;
+
     if (this.attributeKey !== "transform") {
-      return this.context.window.getComputedStyle(this.element)[
-        this.attributeKey
-      ];
+      return getComputedStyle(this.element)[this.attributeKey];
     }
 
-    const obj = {};
-    const transform = compoAttributes[this.attributeKey];
+    const transformScratch = {};
     const currentTransform = getMatrix2D(this.context.window, this.element);
 
-    for (let i = 0; i < transform.length; i++) {
-      obj[transform[i]] = Object.prototype.hasOwnProperty.call(
-        currentTransform,
-        transform[i]
-      )
-        ? currentTransform[transform[i]]
-        : this.context.window.getComputedStyle(this.element)[transform[i]];
+    for (const attr of compoAttributes[this.attributeKey]) {
+      transformScratch[attr] = hasOwnProperty.call(currentTransform, attr)
+        ? currentTransform[attr]
+        : getComputedStyle(this.element)[attr];
     }
 
-    return obj;
+    return transformScratch;
   }
 
   onGetContext() {
-    const options = {};
-    if (
-      Object.prototype.hasOwnProperty.call(compoAttributes, this.attributeKey)
-    ) {
-      const compoAttribute = compoAttributes[this.attributeKey];
-      options.transform = [];
-      for (let i = 0; i < compoAttribute.length; i++) {
-        if (
-          !Object.prototype.hasOwnProperty.call(
-            this.targetValue,
-            compoAttribute[i]
-          )
-        ) {
+    this.options = {};
+    if (hasOwnProperty.call(compoAttributes, this.attributeKey)) {
+      for (const attr of compoAttributes[this.attributeKey]) {
+        if (!hasOwnProperty.call(this.targetValue, attr)) {
           continue;
         }
-        options.transform = [
-          `${options.transform[0] || ""} ${[compoAttribute[i]]}(${
-            this.initialValue[compoAttribute[i]]
-          })`,
-          `${options.transform[1] || ""} ${[compoAttribute[i]]}(${
-            this.targetValue[compoAttribute[i]]
-          })`,
-        ];
+        this.options.transform = [];
+        this.options.transform[0] ??= "";
+        this.options.transform[1] ??= "";
+        this.options.transform[0] += ` ${[attr]}(${this.initialValue[attr]})`;
+        this.options.transform[1] += ` ${[attr]}(${this.targetValue[attr]})`;
       }
     } else {
-      options[this.attributeKey] = [this.initialValue, this.targetValue];
+      this.options[this.attributeKey] = [this.initialValue, this.targetValue];
     }
+
+    this.context.CSSAnimationLayer ??= {};
+    this.context.CSSAnimationLayer[this.element.dataset.motorcortex2Id] ??= {};
+    this.context.CSSAnimationLayer[this.element.dataset.motorcortex2Id][
+      this.attributeKey
+    ] = {
+      start: this.options[this.attributeKey][0],
+      end: this.options[this.attributeKey][1],
+    };
+    this.checkAnimation();
+  }
+
+  createAnimation() {
+    this.creating = true;
     this.target = this.element.animate(
       [
-        { [this.attributeKey]: options[this.attributeKey][0] },
-        { [this.attributeKey]: options[this.attributeKey][1] },
+        { [this.attributeKey]: this.options[this.attributeKey][0] },
+        { [this.attributeKey]: this.options[this.attributeKey][1] },
       ],
       {
         duration: this.props.duration,
@@ -66,9 +66,31 @@ export default class WAA extends MotorCortex.Effect {
       }
     );
     this.target.pause();
+    this.context.CSSAnimationLayer[this.element.dataset.motorcortex2Id][
+      this.attributeKey
+    ] = {
+      start: this.options[this.attributeKey][0],
+      end: this.options[this.attributeKey][1],
+    };
+    this.creating = false;
   }
 
+  checkAnimation() {
+    const { start, end } =
+      this.context.CSSAnimationLayer[this.element.dataset.motorcortex2Id][
+        this.attributeKey
+      ];
+    if (
+      start !== this.options[this.attributeKey][0] ||
+      end !== this.options[this.attributeKey][1] ||
+      !this.target
+    ) {
+      this.createAnimation();
+    }
+  }
   onProgress(fraction) {
+    if (this.creating) return;
+    this.checkAnimation();
     this.target.currentTime = this.props.duration * fraction;
   }
 }
