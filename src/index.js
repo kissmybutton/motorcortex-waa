eb32d2b88c73f79828d42a21913dd686b7952e59
Theofilos Papadopoulos <theofilos.p@kissmybutton.gr>
2021-11-15 03:39:17 +0200

    fix: multiple incidents with same attribute on same element

--- src/index.js
+++ src/index.js
@@ -1,29 +1,13 @@
-import WAA from './Incidents/Effect';
-
-import {name,version}  from '../package.json'
+import WAA from "./Incidents/Effect";
+const { name, version } = require("../package.json");
 
 export default {
-  npm_name: name, // don't touch this
-  version: version, // don't touch this
+  npm_name: name,
+  version: version,
   incidents: [
     {
       exportable: WAA,
-      name: "WAA", // name your Incident any way you want
-    //   attributesValidationRules: {
-    //     // define your attributeValidationRules so MotorCortex can automatically validate them on instantiation 
-    //     // also so your Incidents are directly embedable to DonkeyClip
-    //     animatedAttrs: {
-    //       type: "object",
-    //       props: {
-    //           attr: {
-    //               type: 'string'
-    //           }
-    //         // validation rules as per [fastest-validator](https://www.npmjs.com/package/fastest-validator) library
-    //       }
-    //     }
-    //   }
+      name: "WAA",
     },
-    
-  
   ],
 };
